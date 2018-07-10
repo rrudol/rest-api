1cf0cc252026b65fb5ebfe893eeccb3840d4d77f
Rafał Rudol <rrrrudol@gmail.com>
2018-07-10 20:28:13 +0200

    delete pas-sword

--- src/database.js
+++ src/database.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://rafal:Ye7DRiqF5ZOuRj09@conejitocluster-dpory.mongodb.net/test', { autoIndex: false });
 mongoose.connect('mongodb://localhost:27017/test', { autoIndex: false });
