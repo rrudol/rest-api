a7f578dc6f35dfd7e551bc0b8d6c8a1749f5c8f3
Rafał Rudol <rrrrudol@gmail.com>
2018-09-26 19:54:40 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const index = require('./controllers/index');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('./database');
-const paginate = require('express-paginate');
-const preconditions = require('express-preconditions');
-    
-const app = express();
-app.use(cors( {origin: 'http://localhost:4000'} ));
+const express = require("express");
+const index = require("./controllers/index");
+const bodyParser = require("body-parser");
+// const cors = require('cors');
+const db = require("./database");
+const paginate = require("express-paginate");
+const preconditions = require("express-preconditions");
 
-app.set('etag', 'strong');
+const app = express();
+// app.use(cors( {origin: 'http://localhost:4000'} ));
+
+app.set("etag", "strong");
 app.use(preconditions());
 app.use(paginate.middleware(10, 50));
-app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.json({ type: "application/json" }));
 
-app.use('/', index);
+app.use("/", index);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,5 @@
 const app = require("./app");
-
 const port = parseInt(process.env.PORT) || 4000;
-
-const path = require("path");
-const Bundler = require("parcel-bundler");
-const bundler = new Bundler(path.resolve(__dirname, "../web/index.html"));
-
-app.use(bundler.middleware());
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
